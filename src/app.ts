e04107cdb2a01696ebb7345f35e96b9c89574bd4
Abhishek Upadhyay <abhishek.upadhyay.eds@gmail.com>
2024-11-12 13:01:37 +0530

    remove .env

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const app=express()
+import db from './config/db'
 
 const port =process.env.PORT || 3000
 app.listen(port,()=>{
